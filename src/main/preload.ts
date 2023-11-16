40749c125ad124ce8f50977464f4ec7270096a10
Yeonuk Hwang <younuk23@gmail.com>
2023-11-16 22:36:02 +0900

    CHORE: change HashTag type identifier to Keyword to make it more generic

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,12 +1,12 @@
 import { contextBridge, ipcRenderer } from 'electron';
 import { CHANNEL } from './channel';
-import { HashTag, URL } from './scrapper/types';
+import { Keyword, URL } from './scrapper/types';
 
 export const API = {
   [CHANNEL.LOGIN]: (userInfo: { userName: string; password: string }) =>
     ipcRenderer.invoke(CHANNEL.LOGIN, userInfo),
 
-  [CHANNEL.SCRAP]: (hashTags: HashTag[], urls: URL[]) =>
+  [CHANNEL.SCRAP]: (hashTags: Keyword[], urls: URL[]) =>
     ipcRenderer.invoke(CHANNEL.SCRAP, hashTags, urls),
 
   [CHANNEL.OPEN_FILE]: (path: string) =>
