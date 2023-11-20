acb33165c6548461a8511660c729163c2066acbf
Yeonuk Hwang <younuk23@gmail.com>
2023-11-21 05:09:06 +0900

    FEAT: expose naver scrapper api to renderer

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -6,8 +6,17 @@ export const API = {
   [CHANNEL.LOGIN]: (userInfo: { userName: string; password: string }) =>
     ipcRenderer.invoke(CHANNEL.LOGIN, userInfo),
 
-  [CHANNEL.SCRAP]: (hashTags: Keyword[], urls: URL[]) =>
-    ipcRenderer.invoke(CHANNEL.SCRAP, hashTags, urls),
+  [CHANNEL.SCRAP]: (keywords: Keyword[], urls: URL[]) =>
+    ipcRenderer.invoke(CHANNEL.SCRAP, keywords, urls),
+
+  [CHANNEL.NAVER_INFLUENCER_SCRAP]: (keywords: Keyword[], urls: URL[]) =>
+    ipcRenderer.invoke(CHANNEL.NAVER_INFLUENCER_SCRAP, keywords, urls),
+
+  [CHANNEL.NAVER_CAFE_SCRAP]: (keywords: Keyword[], urls: URL[]) =>
+    ipcRenderer.invoke(CHANNEL.NAVER_CAFE_SCRAP, keywords, urls),
+
+  [CHANNEL.NAVER_BLOG_SCRAP]: (keywords: Keyword[], urls: URL[]) =>
+    ipcRenderer.invoke(CHANNEL.NAVER_BLOG_SCRAP, keywords, urls),
 
   [CHANNEL.OPEN_FILE]: (path: string) =>
     ipcRenderer.invoke(CHANNEL.OPEN_FILE, path),
