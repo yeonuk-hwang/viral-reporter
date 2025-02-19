368cec01ea549271f572dca03d10a77bbd7fcfdd
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-02-19 10:29:31 +0900

    feat: modify top 10 list filtering method in naver influencer service
    related to:
    - d0dc1ebf

--- src/main/naver/NaverInfluencerService.ts
+++ src/main/naver/NaverInfluencerService.ts
@@ -22,28 +22,25 @@ export class InfluencerService extends NaverServiceBase {
   ) {
     const postID = this.extractBlogPostIDFromPostURL(postURL);
 
-    /*
-    상위 10개의 게시물 내에서 검색을 해야 하는데 인플루언서 탭의 경우
-    일부 검색어에서 추천 포스트(.type_join)를 보여주는 경우가 생김
-    이 경우 nth-child에 해당 추천 포스트가 포함되기 때문에
-    단순히 10개까지의 child만 대상으로 검색하게 되면
-    추천 포스트만큼 숫자가 줄어듬
-    예를 들어, 추천 포스트가 2개있다면 10개의 자식 포스트를 검색하게 된다면
-    실질적으로 추천포스트를 제외하고는 상위 8개의 검색어만 대상으로 진행하게 됨
-    이를 해결하기 위해서 추천 포스트의 개수를 파악한 후
-    nth-child(-n+10)에서 추천 포스트 개수만큼 더해줌
-    */
-    const recommendedPostCount = (
-      await $postList.$$(':scope > li.type_join:nth-child(-n+10)')
-    ).length;
+    const $top10_posts = (
+      await $postList.$$(':scope > li:has(div.title_area):not(.type_join)')
+    ).slice(0, 10);
 
-    const $post = await $postList.$(
-      `:scope > li:has(div.title_area > a[data-foryou-gdid*="${postID}"]):not(.type_join):nth-child(-n+${
-        10 + recommendedPostCount
-      })`
-    );
+    try {
+      const $post = await Promise.any(
+        $top10_posts.map(async (target) => {
+          const isPostMatch = await target.$(
+            `a[data-foryou-gdid*="${postID}"]`
+          );
 
-    return $post ? $post.toElement('li') : null;
+          return isPostMatch ? target : Promise.reject();
+        })
+      );
+
+      return $post.toElement('li');
+    } catch {
+      return null;
+    }
   }
 
   private extractBlogPostIDFromPostURL(postURL: string): PostID {
